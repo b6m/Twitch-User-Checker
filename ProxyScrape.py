6d0214bb10686bade36275e098a32c986147afcb
b6m <65096390+b6m@users.noreply.github.com>
2022-03-18 19:42:16 +0100

    Update ProxyScrape.py

--- ProxyScrape.py
+++ ProxyScrape.py
@@ -3,21 +3,22 @@ import colorama
 from colorama import Fore, init
 import time
 
-
 def scrape():
     init(convert=True)
-    f = open("./proxies.txt", "w+")
-    proxies = open("./proxies.txt").read().splitlines()
+    ProxyAmount = 0
     try:
+
+        f = open("./proxies.txt", "w+")
         base_url = "https://api.proxyscrape.com/?request=displayproxies&proxytype=http&timeout=5000"
         r = requests.get(base_url)
         for x in r.text.split("\n"):
             x = x.strip()
             if x:
+                ProxyAmount = ProxyAmount + 1 
                 f.write(str(x)+"\n")
         f.close()
         print(
-            f"{Fore.GREEN}[{Fore.RESET}+{Fore.GREEN}]{Fore.RESET} Sucessfully Scraped Proxies"
+            f"{Fore.GREEN}[{Fore.RESET}+{Fore.GREEN}]{Fore.RESET} Sucessfully Scraped {ProxyAmount} Proxies"
             )
     except:
         print(
